09a90466ec43cefbb5e1334536443900dfd7d74d
Vincent Girard <vinccool96@gmail.com>
2024-01-07 13:31:42 -0500

    progress simple parse

--- packages/core/src/parse.ts
+++ packages/core/src/parse.ts
@@ -1,10 +1,44 @@
 import { CirJsonParseContext } from "./internal/CirJsonParseContext"
+import { CirJsonError } from "./errors/CirJsonError"
 
 export function parse<T>(text: string): T {
   const context = new CirJsonParseContext()
-  return parseUnknown(text, context) as T
+  const baseParsed = JSON.parse(text)
+  return parseUnknown(baseParsed, context) as T
 }
 
-function parseUnknown(text: string, context: CirJsonParseContext): unknown {
+function parseUnknown(baseParsed: unknown, context: CirJsonParseContext): unknown {
+  if (typeof baseParsed === "object" && baseParsed !== null) {
+    return parseObjectOrArray(baseParsed, context)
+  }
+
+  return baseParsed
+}
+
+function parseObjectOrArray(baseParsed: object, context: CirJsonParseContext) {
+  if (Array.isArray(baseParsed)) {
+    return parseArray(baseParsed, context)
+  }
+
   return
+}
+
+function parseArray(array: Array<unknown>, context: CirJsonParseContext) {
+  if (array.length === 0 || typeof array[0] !== "string" || array[0].length === 0) {
+    throw new CirJsonError("The found array without ID at the start")
+  }
+
+  const arrayId = array[0]
+
+  if (context.isIdAlreadyRegistered(arrayId)) {
+    return context.getStructureFromId(arrayId)!
+  }
+
+  const result: Array<unknown> = []
+  context.registerStructure(arrayId, result)
+  const realArray: Array<unknown> = array.slice(1)
+  for (const realArrayElement of realArray) {
+  }
+
+  return result
 }
